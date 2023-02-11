94147143f749c29bc2d5caf7a947ff95f1c7db87
Laurens Kropf <laurens.kropf@kropf-it.at>
2023-02-11 17:10:36 +0100

    added all private networks

--- index.ts
+++ index.ts
@@ -36,7 +36,23 @@ function isLocalNetwork(hostname = window.location.host) {
         hostname.startsWith('localhost') ||
         hostname.startsWith('127.0.0.1') ||
         hostname.startsWith('192.168.') ||
-        hostname.startsWith('10.0.') ||
+        hostname.startsWith('172.16.') ||
+        hostname.startsWith('172.17.') ||
+        hostname.startsWith('172.18.') ||
+        hostname.startsWith('172.19.') ||
+        hostname.startsWith('172.20.') ||
+        hostname.startsWith('172.21.') ||
+        hostname.startsWith('172.22.') ||
+        hostname.startsWith('172.23.') ||
+        hostname.startsWith('172.24.') ||
+        hostname.startsWith('172.25.') ||
+        hostname.startsWith('172.26.') ||
+        hostname.startsWith('172.27.') ||
+        hostname.startsWith('172.28.') ||
+        hostname.startsWith('172.29.') ||
+        hostname.startsWith('172.30.') ||
+        hostname.startsWith('172.31.') ||
+        hostname.startsWith('10.') ||
         hostname.endsWith('.local')
     );
 }
