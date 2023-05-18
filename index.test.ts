5f566193beebf689d63326b7499163d0103ff1ea
David Garner <davidgarner@bulb.co.uk>
2023-05-18 12:18:33 +0100

    fix tests

--- index.test.ts
+++ index.test.ts
@@ -30,7 +30,7 @@ describe('host is corrupted (is empty somehow)', () => {
     expect(protocol).toBe('http:')
     expect(host).toBe('localhost:9000')
   })
-  
+
   test('only from local network passed in', () => {
     window.location.host = '192.168.88.156:4000'
     const { protocol, host, origin } = nextAbsoluteUrl(
@@ -39,7 +39,7 @@ describe('host is corrupted (is empty somehow)', () => {
     )
     expect(origin).toBe('http://192.168.88.156:4000')
     expect(protocol).toBe('http:')
-    expect(host).toBe('localhost:4000')
+    expect(host).toBe('192.168.88.156:4000')
   })
 
   test('only from 127.0.0.1 passed in', () => {
@@ -50,9 +50,9 @@ describe('host is corrupted (is empty somehow)', () => {
     )
     expect(origin).toBe('http://127.0.0.1:4000')
     expect(protocol).toBe('http:')
-    expect(host).toBe('localhost:4000')
+    expect(host).toBe('127.0.0.1:4000')
   })
-  
+
   test('both arguments are passed in', () => {
     const req = {
       headers: {
@@ -161,7 +161,6 @@ describe('host is localhost', () => {
   })
 })
 
-
 describe('behind a proxy', () => {
   test('should use the x-forwarded headers', () => {
     const req = {
@@ -180,5 +179,4 @@ describe('behind a proxy', () => {
     expect(protocol).toBe('http:')
     expect(host).toBe('localhost:5000')
   })
-
 })
